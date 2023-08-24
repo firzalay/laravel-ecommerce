dea9a884e39acdca9463e49be53fe1f86dd6e384
firzalay <firza7004@gmail.com>
2023-08-24 21:07:20 +0700

    feat(admin) : add confirmation before delete category

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -67,7 +67,7 @@
                         
                     <tr>
                         <td>{{ $data->category_name }}</td>
-                        <td><a class="btn btn-danger" href="{{ url("/delete_category/{$data->id}") }}">Delete</a></td>
+                        <td><a onclick="return confirm('Are you sure to delete this?')"   class="btn btn-danger" href="{{ url("/delete_category/{$data->id}") }}">Delete</a></td>
                     </tr>
 
                     @endforeach
