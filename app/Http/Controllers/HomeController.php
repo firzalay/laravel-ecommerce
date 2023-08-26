45a802a1c382fe67dbc9f1de348dd9acd4fee8f5
firzalay <firza7004@gmail.com>
2023-08-26 12:11:56 +0700

    feat(home) : add products to cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,17 +7,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
+use App\Models\Cart;
 
 
 class HomeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::paginate(10);
-    
+
         return view('home.userpage', compact('products'));
     }
 
-    public function redirect() {
+    public function redirect()
+    {
         $usertype = Auth::user()->usertype;
         $products = Product::paginate(10);
 
@@ -25,7 +28,7 @@ class HomeController extends Controller
         if ($usertype == '1') {
             return view('admin.home');
         } else {
-            return view('home.userpage',compact('products'));
+            return view('home.userpage', compact('products'));
         }
     }
 
@@ -36,9 +39,35 @@ class HomeController extends Controller
         return view('home.product_details', compact('product'));
     }
 
-    public function add_cart($id) 
+    public function add_cart(Request $request, $id)
     {
         if (Auth::id()) {
+
+            $user = Auth::user();
+
+            $product = Product::find($id);
+
+            $cart = new Cart;
+
+            $cart->name = $user->name;
+            $cart->email = $user->email;
+            $cart->phone = $user->phone;
+            $cart->address = $user->address;
+            $cart->user_id = $user->id;
+            $cart->product_title = $product->title;
+
+            if ($product->discount_price != null) {
+                $cart->price = $product->discount_price * $request->quantity;
+            } else {
+                $cart->price = $product->price * $request->quantity;
+            }
+
+            $cart->image = $product->image;
+            $cart->product_id = $product->id;
+            $cart->quantity = $request->quantity;
+
+            $cart->save();
+
             return redirect()->back();
         } else {
             return redirect('login');
